5d8ce709b52743be38c8e63a531af054408fe101
Jonathan Kelley <jonathan.kelley@gmail.com>
2017-01-18 21:37:47 -0600

    Exclude orders with a prefix of D

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Shipment.class_eval do
   scope :exportable, -> {
-	joins(:order).where('spree_shipments.state != ?', 'pending')
+	joins(:order).where('spree_shipments.state != ?', 'pending').where("#{Spree::Order.table_name}.number not ilike 'D%'")
   }
 
   def self.between(from, to)
